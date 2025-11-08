19c4f13104640774851578a65aac6bea35d8954e
corax73 <ramaz999@inbox.ru>
2025-11-08 23:02:11 +0400

    work

--- first.c
+++ first.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    int num, k, first, second, third, fourth, result;
+    int num;
+    char res[] = " лет", str0[] = " год", str1[] = " года", strStart[] = "Мне ";
 
-    scanf("%d%d", &k, &num);
-    switch (k)
+    scanf("%d", &num);
+    if (!(num % 100 >= 11 && num % 100 <= 19))
     {
-    case 1:
-        result = num;
-        break;
-    case 2:
-        first = num / 10;
-        second = num % 10;
-        result = first * second;
-        break;
-    case 3:
-        first = num / 100;
-        second = (num % 100) / 10;
-        third = (num % 10) % 10;
-        result = first * second * third;
-        break;
-    case 4:
-        first = num / 1000;
-        second = (num % 1000) / 100;
-        third = (num % 100) / 10;
-        fourth = num % 10;
-        result = first * second * third * fourth;
-        break;
+        switch (num % 10)
+        {
+        case 1:
+            strcpy(res, str0);
+            break;
+        case 2:
+        case 3:
+        case 4:
+            strcpy(res, str1);
+            break;
+        }
     }
-    printf("%d\n", result);
+    printf("%s%d%s\n", strStart, num, res);
 
     return 0;
 }
