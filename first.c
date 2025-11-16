452f8b881df5829de4a7ee95083e164fd3ae065e
corax73 <ramaz999@inbox.ru>
2025-11-16 23:24:34 +0400

    work

--- first.c
+++ first.c
@@ -2,18 +2,15 @@
 
 int main(void)
 {
-    int base_damage, armor_class, is_critical;
-    double defender_health, res;
+    int num0, num1;
 
-    scanf("%d%lf%d%d", &base_damage, &defender_health, &armor_class, &is_critical);
+    scanf("%d%d", &num0, &num1);
 
-    res = base_damage * (is_critical ? 2 : 1);
-    res = res - (armor_class > 0 ? res * armor_class / 10 : 0);
-    printf("%.2f\n", res);
-    if (res > defender_health)
+    for (int i = num1; i >= num0; i--)
     {
-        printf("You win!\n");
+        printf("%d%s", i, (i > (num0) ? " " : ""));
     }
 
+    printf("\n");
     return 0;
 }
