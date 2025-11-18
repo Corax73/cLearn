b9715cb548ebdfca42a2f4024086d94447c7ea10
corax73 <ramaz999@inbox.ru>
2025-11-18 23:24:07 +0400

    work

--- first.c
+++ first.c
@@ -2,15 +2,22 @@
 
 int main(void)
 {
-    int num0, num1;
+    int num0, num1 = 0, num2 = 1, res= num1 + num2;
 
-    scanf("%d%d", &num0, &num1);
-
-    for (int i = num1; i >= num0; i--)
+    scanf("%d", &num0);
+    if (num0 <= 2)
     {
-        printf("%d%s", i, (i > (num0) ? " " : ""));
+        res = 1;
     }
-
-    printf("\n");
+    else
+    {
+        for (int i = 2; i < num0; i++)
+        {
+            num1 = num2;
+            num2 = res;
+            res = num2 + num1;
+        }
+    }
+    printf("%d\n", res);
     return 0;
 }
