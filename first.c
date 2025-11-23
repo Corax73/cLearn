c565e2e5ac28bf22a28ffa254e137e9d2f2d4bb4
corax73 <ramaz999@inbox.ru>
2025-11-23 23:15:11 +0400

    work

--- first.c
+++ first.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(void)
 {
-    long num0 = 0, num1 = 0;
-    int num2 = 1, num3 = 2;
+    long num0 = 0;
+    int num1 = 0, num2 = 10;
 
     scanf("%ld", &num0);
-    num1 = pow(num3, num2);
-    if (num1 > num0)
+    num1 = num0 % num2;
+
+    if (num1 == num0)
     {
+        printf("%ld\n", num0);
         return 0;
     }
-    while (num1 <= num0)
+    printf("%d", num1);
+    num2 *= 10;
+    while (num1 != num0)
     {
-        num1 = pow(num3, num2);
-        if (num1 >= num0)
-        {
-            if (num1 == num0)
-            {
-                printf("%d ", num2);
-            }
-            break;
-        }
-        printf("%d ", num2);
-        num2++;
+        num1 = num0 % num2;
+        printf("%d", num1 / (num2 > 10 ? (num2 / 10) : num2));
+        num2 *= 10;
     }
     return 0;
 }
