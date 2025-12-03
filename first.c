774217187b5917ab0c7c4ddc14b520ca0ade1078
corax73 <ramaz999@inbox.ru>
2025-12-03 23:24:03 +0400

    work

--- first.c
+++ first.c
@@ -2,37 +2,30 @@
 
 int main(void)
 {
-    int num0 = 0;
-    double summ = 0, aMean = 0;
-    scanf("%d", &num0);
+    int num0 = 0, num1 = 0;
+    scanf("%d %d", &num0, &num1);
 
-    int arr[num0];
-
-    for (int i = 0; i < num0; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
+    int arr[num0][num1];
 
     for (int i = 0; i < num0; i++)
     {
-        summ += arr[i];
-    }
-    aMean = summ / num0;
-    for (int i = 0; i < num0; i++)
-    {
-        if (arr[i] > aMean)
+        for (int j = 0; j < num1; j++)
         {
-            printf("%d ", arr[i]);
+            scanf("%d", &arr[i][j]);
         }
     }
     for (int i = 0; i < num0; i++)
     {
-        if (arr[i] <= aMean)
+        for (int j = 0; j < num1; j++)
         {
-            printf("%d", arr[i]);
-            if (i != num0 - 1)
+            printf("%d", arr[i][j]);
+            if (j != num1 - 1)
             {
                 printf(" ");
+            }
+            if (j == num1 - 1)
+            {
+                printf("\n");
             }
         }
     }
