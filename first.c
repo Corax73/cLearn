b73f582d2d8e3b8ab4904d91fd1ce54668af603d
corax73 <ramaz999@inbox.ru>
2025-12-07 00:06:49 +0400

    work

--- first.c
+++ first.c
@@ -2,28 +2,49 @@
 
 int main(void)
 {
-    int num0 = 0, num1 = 0;
-    long long summ = 0;
-    scanf("%d %d", &num0, &num1);
+    int num0 = 0, temp = 0, index = 0;
+    long long k = 0, offset = 0;
+    scanf("%d", &num0);
 
-    int arr[num0][num1];
+    int arr0[num0][num0];
+    int arr1[num0][num0];
 
     for (int i = 0; i < num0; i++)
     {
-        for (int j = 0; j < num1; j++)
+        for (int j = 0; j < num0; j++)
         {
-            scanf("%d", &arr[i][j]);
+            scanf("%d", &arr0[i][j]);
         }
     }
-    for (int j = num1 - 1; j >= 0; j--)
+    scanf("%lld", &k);
+    if (k > num0)
     {
-        summ = 0;
-        for (int i = 0; i < num0; i++)
-        {
-            summ += arr[i][j];
-        }
-        printf("%lld ", summ);
+        offset = k % num0;
     }
-    printf("\n");
+    else
+    {
+        offset = k;
+    }
+    for (int i = 0; i < num0; i++)
+    {
+        for (int j = 0; j < num0; j++)
+        {
+            index = j + offset;
+            if (index > num0 - 1)
+            {
+                index = index % num0;
+            }
+            arr1[i][index] = arr0[i][j];
+        }
+    }
+    for (int i = 0; i < num0; i++)
+    {
+
+        for (int j = 0; j < num0; j++)
+        {
+            printf("%d ", arr1[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
