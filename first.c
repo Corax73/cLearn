0f300b5cb8342352e4be70b6f09370e8bb2657a7
corax73 <ramaz999@inbox.ru>
2025-12-17 22:56:00 +0400

    work

--- first.c
+++ first.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
 int main(void)
 {
-    int size = 54;
-    char city0[size], city1[size], yes[] = "yes", no[] = "no";
-    scanf("%s", city0);
-    scanf("%s", city1);
+    int size = 54, offset = 97, isMatched = 1;
+    int arr0[26] = {0}, arr1[26] = {0};
+    char str0[size], str1[size], yes[] = "yes", no[] = "no";
+    scanf("%[^\n]%*c", str0);
+    scanf("%[^\n]%*c", str1);
 
-    if (city0[strlen(city0) - 1] == tolower(city1[0]) || city1[strlen(city1) - 1] == tolower(city0[0]))
+    for (int i = 0; i < size; i++)
+    {
+        if ((str0[i] == '\\' && str0[i + 1] == 'n') || str0[i] == '\n' || str0[i] == '\0')
+        {
+            break;
+        }
+        arr0[str0[i] - offset]++;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        if ((str1[i] == '\\' && str1[i + 1] == 'n') || str1[i] == '\n' || str1[i] == '\0')
+        {
+            break;
+        }
+        arr1[str1[i] - offset]++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (arr0[i] < arr1[i])
+        {
+            isMatched = 0;
+            break;
+        }
+    }
+
+    if (isMatched)
     {
         printf("%s\n", yes);
     }
