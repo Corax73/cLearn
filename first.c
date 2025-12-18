751f216c5f1e20000df5bcd6e8feb2e0fd96f44a
corax73 <ramaz999@inbox.ru>
2025-12-18 23:02:53 +0400

    work

--- first.c
+++ first.c
@@ -1,47 +1,26 @@
 #include <stdio.h>
 
+int factorial(int start)
+{
+    int res = 1;
+    for (int i = 1; i <= start; i++)
+    {
+        res *= i;
+    }
+    return res;
+}
+
+int combin(int n, int k)
+{
+    return factorial(n) / (factorial(k) * (factorial(n - k)));
+}
+
 int main(void)
 {
-    int size = 54, offset = 97, isMatched = 1;
-    int arr0[26] = {0}, arr1[26] = {0};
-    char str0[size], str1[size], yes[] = "yes", no[] = "no";
-    scanf("%[^\n]%*c", str0);
-    scanf("%[^\n]%*c", str1);
+    int n = 0, k = 0;
+    scanf("%d%d", &n, &k);
 
-    for (int i = 0; i < size; i++)
-    {
-        if ((str0[i] == '\\' && str0[i + 1] == 'n') || str0[i] == '\n' || str0[i] == '\0')
-        {
-            break;
-        }
-        arr0[str0[i] - offset]++;
-    }
+    printf("%d\n", combin(n, k));
 
-    for (int i = 0; i < size; i++)
-    {
-        if ((str1[i] == '\\' && str1[i + 1] == 'n') || str1[i] == '\n' || str1[i] == '\0')
-        {
-            break;
-        }
-        arr1[str1[i] - offset]++;
-    }
-
-    for (int i = 0; i < 26; i++)
-    {
-        if (arr0[i] < arr1[i])
-        {
-            isMatched = 0;
-            break;
-        }
-    }
-
-    if (isMatched)
-    {
-        printf("%s\n", yes);
-    }
-    else
-    {
-        printf("%s\n", no);
-    }
     return 0;
 }
