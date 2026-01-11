19537eaf3c9c1d25a46f992654d3c71d16a7ab62
corax73 <ramaz999@inbox.ru>
2026-01-11 22:43:06 +0400

    work

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <string>
-#include <format>
 
 using namespace std;
 
 int main()
 {
-    string s;
-    cin >> s;
-    
-    cout << std::format("Привет, {}!", s) << endl; 
-};
+    int num0 = 0, check = 0;
+    string err = "Ошибка", numErr = "Цифры числа различаются", win = "Вы выиграли!";
+    cin >> num0;
+
+    if (num0 < 10 || num0 > 99)
+    {
+        cout << err << endl;
+    }
+    else
+    {
+        if (num0 / 10 != num0 % 10)
+        {
+            cout << numErr << endl;
+        }
+        else
+        {
+            cout << win << endl;
+        }
+    }
+}
