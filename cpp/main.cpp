8017c08abb513d294793d829c7862fe64ff7c44e
corax73 <ramaz999@inbox.ru>
2026-01-13 23:08:57 +0400

    work

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-int main()
-{
-    int x = 0, y = 0;
-    string err0 = "Неверная строка", err1 = "Неверный столбец";
-    string arr[3][3] = {
-        {"Superman", "Batman", "Spiderman"},
-        {"Ironman", "Wolverine", "Aquaman"},
-        {"Thor", "Hulk", "Flash"},
-    };
-
-    cin >> x >> y;
-
-    if (x > 3 || x == 0)
+int main() {
+    int x = 0, isSimple = 1;
+    cin >> x;
+    if (x > 1)
     {
-        cout << err0 << endl;
+        for (int i = 2; i < x; i++)
+        {
+            if (x % i == 0)
+            {
+                isSimple = 0;
+                break;
+            }
+        }
+    } else {
+        isSimple = 0;
     }
-    else if (y > 3 || y == 0)
-    {
-        cout << err1 << endl;
-    }
-    else
-    {
-        cout << arr[x - 1][y - 1] << endl;
-    }
+    cout << (isSimple ? "yes" : "no") << endl;
 }
