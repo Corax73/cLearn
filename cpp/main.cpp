5d1a604b00a11a730408c1f527e8303e9282cde2
corax73 <ramaz999@inbox.ru>
2026-01-21 23:41:01 +0400

    work

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,21 +3,24 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
+    int num0 = 0;
+
+    cin >> num0;
+
+    double a[num0], max = 0;
+
+    for (int i = 0; i < num0; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            if ((i + j) % 2 == 0)
-            {
-                cout << "x";
-            }
-            else
-            {
-                cout << "o";
-            }
-        }
-        cout << endl;
+        cin >> a[i];
     }
+
+    max = a[0];
+    for (int i = 0; i < num0; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+    }
+    cout << max << endl;
 }
