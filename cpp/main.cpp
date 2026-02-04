b0bf720402f801b62405c3d2bf2735436757b767
corax73 <ramaz999@inbox.ru>
2026-02-04 23:54:57 +0400

    work

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,70 +1,22 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-void diamond(int size)
+void reverse(string &str)
 {
-    int width = (size * 2) - 1;
-    int middle = width / 2;
-    int left = middle, right = middle;
-    for (int i = 0; i < width; i++)
+    for (int i = 0; i < str.size() / 2; i++)
     {
-        if (i <= middle)
-        {
-            for (int j = 0; j < left; j++)
-            {
-                cout << ' ';
-            }
-            for (int j = left; j < right + 1; j++)
-            {
-                cout << '*';
-            }
-            for (int j = right + 1; j < width; j++)
-            {
-                cout << ' ';
-            }
-            cout << endl;
-            if (i != middle)
-            {
-                if (left > 0)
-                {
-                    left--;
-                }
-                if (right < width)
-                {
-                    right++;
-                }
-            }
-        }
-        else
-        {
-            if (left < width)
-            {
-                left++;
-            }
-            if (right > 0)
-            {
-                right--;
-            }
-            for (int j = left; j > 0; j--)
-            {
-                cout << ' ';
-            }
-            for (int j = left; j < right + 1; j++)
-            {
-                cout << '*';
-            }
-            for (int j = right + 1; j < width; j++)
-            {
-                cout << ' ';
-            }
-            cout << endl;
-        }
+        char temp = str[i];
+        str[i] = str[str.size() - 1 - i];
+        str[str.size() - 1 - i] = temp;
     }
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    diamond(n);
+    string s;
+    getline(cin, s);
+    reverse(s);
+    cout << s;
 }
