004894dbab771fd9baf326b94d8698e110520150
corax73 <ramaz999@inbox.ru>
2026-02-06 23:27:29 +0400

    work

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-void reverse(string &str)
+void sort(int arr[], int n)
 {
-    for (int i = 0; i < str.size() / 2; i++)
+    if (n > 0)
     {
-        char temp = str[i];
-        str[i] = str[str.size() - 1 - i];
-        str[str.size() - 1 - i] = temp;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 }
 
 int main()
 {
-    string s;
-    getline(cin, s);
-    reverse(s);
-    cout << s;
+    int size = 0;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i];
+        if (i < size - 1)
+        {
+            cout << ' ';
+        }
+    }
+    cout << endl;
 }
